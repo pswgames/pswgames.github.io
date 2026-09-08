ff985bc24f9ebc801675444371fbce4caee9aebf
kkokmatch <hanarin0201@gmail.com>
2026-09-08 18:10:50 +0900

    Use generated city photo in elevator view

--- js/v5-view.js
+++ js/v5-view.js
@@ -1,21 +1,14 @@
 (()=>{'use strict';
-function realisticLandscape(){return `<div class="realistic-landscape" aria-hidden="true">
-  <div class="rl-sky"></div><div class="rl-sun"></div><div class="rl-cloud rl-c1"></div><div class="rl-cloud rl-c2"></div><div class="rl-cloud rl-c3"></div>
-  <div class="rl-mountain rl-m1"></div><div class="rl-mountain rl-m2"></div><div class="rl-haze"></div>
-  <div class="rl-city far">${Array.from({length:15},(_,i)=>`<i style="--i:${i};--h:${42+(i*23)%120}px"></i>`).join('')}</div>
-  <div class="rl-city near">${Array.from({length:12},(_,i)=>`<i style="--i:${i};--h:${85+(i*37)%210}px"></i>`).join('')}</div>
-  <div class="rl-river"><span></span></div><div class="rl-bridge"></div>
-  <div class="rl-park"></div><div class="rl-road"><span></span></div>
-  <div class="rl-foreground"><i></i><i></i><i></i><i></i><i></i><i></i><i></i></div>
-</div>`}
+function photoLandscape(){return `<div class="realistic-landscape photo-landscape" aria-hidden="true"></div>`}
 function enhance(){
   const shell=document.querySelector('.elevator-shell');
   if(!shell)return;
   shell.querySelector('.controls')?.remove();
   const track=shell.querySelector('#floorTrack');
-  if(track&&!track.querySelector('.realistic-landscape')){
+  if(track&&!track.querySelector('.photo-landscape')){
     track.querySelector('.scenic-world')?.setAttribute('hidden','');
-    track.insertAdjacentHTML('afterbegin',realisticLandscape());
+    track.querySelector('.realistic-landscape')?.remove();
+    track.insertAdjacentHTML('afterbegin',photoLandscape());
   }
 }
 const main=document.querySelector('#main');
