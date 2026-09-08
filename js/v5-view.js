814343f3bb16c424cc7c886ee8482d64d8d6b294
kkokmatch <hanarin0201@gmail.com>
2026-09-08 18:18:49 +0900

    Stack generated city image strips in elevator

--- js/v5-view.js
+++ js/v5-view.js
@@ -1,5 +1,10 @@
 (()=>{'use strict';
-function photoLandscape(){return `<div class="realistic-landscape photo-landscape" aria-hidden="true"></div>`}
+function photoLandscape(){return `<div class="realistic-landscape photo-landscape" aria-hidden="true">
+  <i class="city-strip city-strip-1"></i>
+  <i class="city-strip city-strip-2"></i>
+  <i class="city-strip city-strip-3"></i>
+  <i class="city-strip city-strip-4"></i>
+</div>`}
 function enhance(){
   const shell=document.querySelector('.elevator-shell');
   if(!shell)return;
